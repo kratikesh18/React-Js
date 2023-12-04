f286fcc3171a7a4aed59635f2bd2906a05571d93
kratikesh18 <kartikpachkawade@gmail.com>
2023-12-05 01:38:15 +0530

    Added custom react modifications

--- 02_react-with-vite/src/App.jsx
+++ 02_react-with-vite/src/App.jsx
@@ -1,8 +1,13 @@
 
-function App() {
 
+//the app is the function only so we can run it by calling it directly  lets try NOT RECOMMENDED
+
+//using variables in jsx
+function App() {
+  const username = "Kartikesh"
   return (
-   <h1>Hello Kartikesh From React with Vite!</h1>
+   <h1>Hello {username} From React with Vite!</h1>
+        //this is not like template literals we can't write whole js here , this is only for evaluated expressions 
   )
 }
 
--- 02_react-with-vite/src/main.jsx
+++ 02_react-with-vite/src/main.jsx
@@ -2,15 +2,63 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>This is from Custom React Learnings!</h1>
+    </div>
+  )
+}
+
+
+//so how the react creates the elements 
+const customReactEle = { 
+  type: "a", 
+  properties:{
+      href:"https://www.linkedin.com",
+      target:'_blank'
+  },
+  innerChild: "Click me to visit LinkedIn"
+}
+
+//this is also an element /component whatever idk  
+const AnotherCustomEle =(
+  <a href="https://linkedin.com" target= "_blank">visit me </a>
+)
+
+
+//creating the element using React.creatElement() method 
+
+const finalCustomEle = React.createElement(
+  //it will first take the tagname or type of ele
+  'a',  //then the object of the props 
+  {href:"https://github.com", target:"_blank"},
+  //lastly the innerHTML of the element 
+  "Click me I'm final custom element"
+) 
+
+
+
 // in in case we need an component to run  in brower we need to immport it 
 import Wet from './Wet.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   // those are Strict modes useed for perfections you can delete it either
-  // <React.StrictMode>
-  //this will return only one element so we can wrap it into the div tag or in the fragment 
-  <div>
-    <App />
-    <Wet />
-  </div>
-  // </React.StrictMode>,
+  //this will return only one element so we can wrap it into the div tag or in the fragment
+
+  // <div>
+
+  //   <MyApp/>
+  //   <App />
+  //   <Wet /> 
+  // </div>
+
+  //custom React code 
+  //MyApp() //we can do this but not any fragment should be present here
+          //and THIS ACTION IS NOT RECOMENDED AT ALL 
+  // customReactEle  
+  //AnotherCustomEle  //we can get it as well 
+  //finalCustomEle
+  <>
+  <App/>
+  </>
 )
