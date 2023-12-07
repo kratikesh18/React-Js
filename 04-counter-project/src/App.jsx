59a70c3a6750f7de10cf18d6fd1074966bf41d36
kratikesh18 <kartikpachkawade@gmail.com>
2023-12-07 16:36:23 +0530

    updated counter project

--- 04-counter-project/src/App.jsx
+++ 04-counter-project/src/App.jsx
@@ -8,16 +8,26 @@ import './App.css'
 
 function App() {
   //the useState hook provide us the array of 2 elements 1 is the variable and 2nd one is a functional component
-  let [counter , setCounter] = useState(18)
+  const [counter , setCounter] = useState(18)
 
   // let counter= 18;
   //creating the function onlick increase the value of the counter
   const addValue =()=>{
     // counter++ ;
-    if(counter == 25){
-            return;
-    }
-    counter = setCounter(counter+1)
+    // if(counter == 25){
+    //         return;
+    // }
+
+      //this is the important question  it will not increase the value directly based upon freq of setCounter
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+    // setCounter(counter+1)
+
+    //instead setCounter expects a callback which takes prevCount as a argument 
+    setCounter(prevCount => prevCount+1)
+    setCounter(prevCount => prevCount+1)
+    setCounter(prevCount => prevCount+1)
     // console.log(`Counter incresed by 1: ${counter}` )
   }
   const removeValue =()=>{
@@ -26,7 +36,7 @@ function App() {
       
       return;
     }
-    counter = setCounter(counter-1)
+    setCounter(counter-1)
     // console.log(`Counter decreased by 1: ${counter}` )
   }
   //now if we want to display all the values to the ui thats where the hooks will used
