1fc03175b3aafe6011de22f7944f41e9066b9cab
kratikesh18 <im.kartikesh18@gmail.com>
2024-01-01 22:45:18 +0530

    modified password generator file

--- 12-ToDo-List-Context-API/src/App.jsx
+++ 12-ToDo-List-Context-API/src/App.jsx
@@ -40,14 +40,17 @@ useEffect(()=>{
   localStorage.setItem('todos' , JSON.stringify(todos))
 } , [todos])
 
-
+// background: rgb(107,10,10);
+//background: linear-gradient(324deg, rgba(107,10,10,1) 0%, rgba(73,11,108,1) 92%);
 
   return (
     <TodoProvider value={{todos, toggleComplete,addTodo, updateTodo,deleteTodo}}>
 
-      <div className="bg-[#172842] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+      <div className="bg-gradient-custom  min-h-screen w-full py-8 flex flex-col justify-between">
+        <div className="w-full max-w-2xl mx-auto shadow-custom rounded-lg px-4 py-3 text-white">
+
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Tasks</h1>
+
           <div className="mb-4">
             {/* Todo form goes here */}
             <TodoForm/>
@@ -63,6 +66,10 @@ useEffect(()=>{
               </div>
             ))}
           </div>
+
+        </div>
+        <div>
+          <h1 className='text-[0.79rem] text-white '>&copy;Kartikesh</h1>
         </div>
       </div>
 
